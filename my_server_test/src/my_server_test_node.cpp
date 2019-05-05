54dd07f900639084c3f0f315cdc229bb3953295c
tuanti1997qn <tuanti1997qn@gmail.com>
2019-05-05 15:35:50 +0700

    them mot dong bug nua can sua

--- my_server_test/src/my_server_test_node.cpp
+++ my_server_test/src/my_server_test_node.cpp
@@ -2,13 +2,18 @@
 #include "math.h"
 #include <tf/transform_listener.h>
 #include "geometry_msgs/Twist.h"
+#include "sensor_msgs/LaserScan.h"
 
 void Robot1_Callback(const geometry_msgs::Twist::ConstPtr &msg);
 void Robot2_Callback(const geometry_msgs::Twist::ConstPtr &msg);
 int is_available(tf::StampedTransform my_transform, float min_dist);
 
+void Robot1_Scan_Callback(const sensor_msgs::LaserScan::ConstPtr &msg);
+
 ros::Publisher r1_pub, r2_pub;
-tf::StampedTransform transform;
+tf::StampedTransform transform, transform_x1_g, transform_x2_g;
+
+sensor_msgs::LaserScan my_scan;
 
 int main(int argc, char **argv)
 {
@@ -21,9 +26,10 @@ int main(int argc, char **argv)
   // phan init cho nhan cmd_vel
 
   // init cho robot1
-  ros::NodeHandle r1_nht, r1_nhr;
+  ros::NodeHandle r1_nht, r1_nhr, r1_nhscan;
   r1_pub = r1_nht.advertise<geometry_msgs::Twist>("/robot1/cmd_vel", 1000);
   ros::Subscriber r1_sub = r1_nhr.subscribe("/robot1/c_cmd_vel", 1000, Robot1_Callback);
+  ros::Subscriber r1_sub_Scan = r1_nhscan.subscribe("/robot1/scan", 1000, Robot1_Scan_Callback);
 
   // init cho robot2
   ros::NodeHandle r2_nht, r2_nhr;
@@ -36,14 +42,43 @@ int main(int argc, char **argv)
     try
     {
       listener.lookupTransform("/robot1/base_footprint", "/robot2/base_footprint", ros::Time(0), transform);
+      listener.lookupTransform("/robot1/base_footprint", "/map", ros::Time(0), transform_x1_g);
+      listener.lookupTransform("/robot2/base_footprint", "/map", ros::Time(0), transform_x2_g);
     }
     catch (tf::TransformException ex)
     {
+    }
+    if (my_scan.ranges.empty() == 0)
+    {
+      std::cout << my_scan.ranges.at(6) << std::endl;
     }
 
     // std::cout << " chi so x " << transform.getOrigin().x() << std::endl;
     // std::cout << " chi so y " << transform.getOrigin().y() << std::endl;
     // std::cout << " chi so z " << transform.getOrigin().z() << std::endl;
+
+    // test zone
+    // robot 1 move
+    geometry_msgs::Twist my_vel;
+    if (is_available(transform, 0.8)) // >0.8
+    {
+    }
+    else if (is_available(transform, 0.6)) // >0.6 , <0.8
+    {
+      /* code */
+      my_vel.linear.x = 0;
+      r1_pub.publish(my_vel);
+    }
+    else // <0.6
+    {
+
+      my_vel.linear.x = -0.2;
+      r1_pub.publish(my_vel);
+    }
+
+
+
+
     ros::spinOnce();
     // rate.sleep();
   }
@@ -54,21 +89,34 @@ void Robot1_Callback(const geometry_msgs::Twist::ConstPtr &msg)
 {
   // tf::TransformListener listener;
   // listener.lookupTransform("/robot1/base_footprint", "/robot2/base_footprint", ros::Time(0), transform);
-  if (is_available(transform,0.5))
+  geometry_msgs::Twist my_vel;
+  if (is_available(transform, 0.8)) // >0.8
   {
     r1_pub.publish(msg);
-    std::cout << "robot 1 move"<< std::endl;
+    std::cout << "robot 1 move" << std::endl;
+  }
+  else if (is_available(transform, 0.6)) // >0.6 , <0.8
+  {
+    /* code */
+    my_vel.linear.x = 0;
+    r1_pub.publish(my_vel);
+  }
+  else // <0.6
+  {
+
+    my_vel.linear.x = -0.2;
+    r1_pub.publish(my_vel);
   }
 }
-  
+
 void Robot2_Callback(const geometry_msgs::Twist::ConstPtr &msg)
 {
   // tf::TransformListener listener;
   // listener.lookupTransform("/robot1/base_footprint", "/robot2/base_footprint", ros::Time(0), transform);
-  if (is_available(transform,0.5))
+  if (is_available(transform, 0.5))
   {
     r2_pub.publish(msg);
-    std::cout << "robot 2 move"<< std::endl;
+    std::cout << "robot 2 move" << std::endl;
   }
 }
 
@@ -97,4 +145,11 @@ int is_available(tf::StampedTransform my_transform, float min_dist)
   min_dist *= min_dist;
 
   return (dist > min_dist);
+}
+
+void Robot1_Scan_Callback(const sensor_msgs::LaserScan::ConstPtr &msg)
+{
+  my_scan.angle_increment = msg->angle_increment;
+  my_scan.ranges = msg->ranges;
+  // std::cout << msg->angle_increment << std::endl;
 }
